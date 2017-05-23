1964de3847fb7f40894e46972d01d9cd6a9aef1b
cash2910 <glory_jzx@sina.com>
2017-05-23 11:23:00 +0800

    Update params.php
    去除配置信息

--- backend/config/params.php
+++ backend/config/params.php
@@ -1,8 +1,4 @@
 <?php
 return [
-    //微信信息  wx12111faaec45e471   7960ee433ee6b027b93a0c2310759323
-/*     'AppId' => 'wxe8bb6f3971a4974c',
-    'AppSecret' =>'fcc2945f5cc07d639d0d79cb0e444c87' */
-    'AppId' => 'wx12111faaec45e471',
-    'AppSecret' =>'7960ee433ee6b027b93a0c2310759323'
+
 ];
