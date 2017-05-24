cd0fa2e6a4cc2b0b8928ef516038be2818e9386b
zaixing.jiang <zaixing.jiang@dalegames.net>
2017-05-24 11:11:57 +0800

    打印日志

--- common/service/weixin/WeChatResponseService.php
+++ common/service/weixin/WeChatResponseService.php
@@ -6,6 +6,7 @@ use yii\base\Exception;
 use service\users\UserService;
 use yii\base\Event;
 use yii;
+use yii\helpers\StringHelper;
 /**
  * 回复微信消息方法
  * @author zaixing.jiang
@@ -21,10 +22,9 @@ class WeChatResponseService extends Module{
         $eventObj = $this->getEvent( $params );
         $event = new Event();
         $event->sender = $eventObj;
-        $eventName = $eventObj->MsgType;
+        $eventName = strtolower( $eventObj->MsgType );
         if( $eventObj->MsgType == 'event' )
-            $eventName = $eventObj->Event;
-        var_dump( $eventName );
+            $eventName = strtolower( $eventObj->Event );
         $this->trigger( $eventName , $event );
         return $eventObj;
         
@@ -39,13 +39,13 @@ class WeChatResponseService extends Module{
         //用户订阅 ...
         $this->on('subscribe', function( $event ){
             $entity = $event->sender;
-            $ret = UserService::getInstance()->createUser([
+/*             $ret = UserService::getInstance()->createUser([
                 'add_time'=> $entity->CreateTime,
                 'event_key'=>$entity->EventKey,
                 'open_id' => $entity->FromUserName,
                 'ticket' => $entity->Ticket
-            ]);
-                        
+            ]); */
+            $entity->setResp("aaaa");
         });
         
         //接收普通消息  text image ...
@@ -54,22 +54,22 @@ class WeChatResponseService extends Module{
         });
         
         //已关注用户扫码行为
-        $this->on('SCAN', function( $event ){
+        $this->on('scan', function( $event ){
             return "aaa";
         });
         
          //上报地理位置事件
-        $this->on('LOCATION', function( $event ){
+        $this->on('location', function( $event ){
             return "aaa";
         });
         
         //自定义菜单事件(点击菜单拉取消息时的事件推送)
-        $this->on('CLICK', function( $event ){
+        $this->on('click', function( $event ){
             return "aaa";
         });
         
         //自定义菜单事件(点击菜单跳转链接时的事件推送)
-        $this->on('VIEW', function( $event ){
+        $this->on('view', function( $event ){
             return "aaa";
         });
     }
@@ -94,7 +94,7 @@ class ProxyXml{
         $this->xml = $xmlObj;
     }
     function __get( $attr ){
-        return isset( $this->xml[$attr] ) ? trim( $this->xml[$attr] ) : "";
+        return isset( $this->xml->$attr ) ? trim( $this->xml->$attr ) : "";
     }
     
     function setResp( $str ){
