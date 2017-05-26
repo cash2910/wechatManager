b908b4a1551306b192ef766353dc3c99431b60c5
zaixing.jiang <zaixing.jiang@dalegames.net>
2017-05-26 11:20:50 +0800

    init

--- environments/dev/common/config/params-local.php
+++ environments/dev/common/config/params-local.php
@@ -1,4 +1,8 @@
 <?php
 return [
-
+    'adminEmail' => 'admin@example.com',
+    'supportEmail' => 'support@example.com',
+    'user.passwordResetTokenExpire' => 3600,
+    'AppId' => 'wx33a109a15b3f3302',
+    'AppSecret'=>'7c3ae345f52e87945cf655c1f24ad1e7'
 ];
