9783b5ad0c6e4d24c7ff052e5cb351b5e9ff67a1
zaixing.jiang <zaixing.jiang@dalegames.net>
2017-05-26 17:03:31 +0800

    init

--- environments/dev/common/config/main-local.php
+++ environments/dev/common/config/main-local.php
@@ -10,7 +10,7 @@ return [
         ],
         'redis' => [
             'class' => 'yii\redis\Connection',
-            'hostname' => 'redis.dlgive.idc.dalegames.com',
+            'hostname' => '127.0.0.1',
             'port' => 6379,
             'database' => 0,
         ],
