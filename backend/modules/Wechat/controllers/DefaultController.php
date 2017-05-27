50705f87496c37ae42b8c08cd959d7ecbcc63e70
zaixing.jiang <zaixing.jiang@dalegames.net>
2017-05-27 13:57:51 +0800

    init

--- backend/modules/Wechat/controllers/DefaultController.php
+++ backend/modules/Wechat/controllers/DefaultController.php
@@ -104,19 +104,19 @@ class DefaultController extends Controller
                  ['item'=>[
                      'Title'=>'王者农药',
                      'Description'=>'王者农药 就是干',
-                     'PicUrl'=> 'https://www.baidu.com',
+                     'PicUrl'=> 'http://imgtg.37wan.com/u/2017/0508/081112549ctt7.jpg',
                      'Url' =>'https://ss1.baidu.com/6ONXsjip0QIZ8tyhnq/it/u=3365950462,3553557768&fm=58'
                  ]],
                  ['item'=>[
                      'Title'=>'王者农药1',
                      'Description'=>'王者农药 就是干',
-                     'PicUrl'=> 'https://www.baidu.com',
+                     'PicUrl'=> 'http://imgtg.37wan.com/u/2017/0502/02111015pXtoF.jpg',
                      'Url' =>'https://ss1.baidu.com/6ONXsjip0QIZ8tyhnq/it/u=3365950462,3553557768&fm=58'
                  ]],
                  ['item'=>[
                      'Title'=>'王者农药2',
                      'Description'=>'王者农药 就是干',
-                     'PicUrl'=> 'https://www.baidu.com',
+                     'PicUrl'=> 'http://imgtg.37wan.com/u/2017/0502/02144626JFkQz.jpg',
                      'Url' =>'https://ss1.baidu.com/6ONXsjip0QIZ8tyhnq/it/u=3365950462,3553557768&fm=58'
                  ]]
              ]
