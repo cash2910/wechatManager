879bd4920af94407cf1d5642c98d2df11a06f9a9
zaixing.jiang <zaixing.jiang@dalegames.net>
2017-05-27 17:40:25 +0800

    init

--- common/service/weixin/WeChatResponseService.php
+++ common/service/weixin/WeChatResponseService.php
@@ -81,8 +81,8 @@ class WeChatResponseService extends Module{
                     if( empty($uInfo)  )
                         throw new Exception(" invalid scran ID");
                     $rel = $uInfo->user_rels;
-                    yii::error($rel);
-                    $model->user_rels = !empty( $rel ) ? ($rel."-".$uInfo->id) : $uInfo->id ;
+                    $model->user_rels = !empty( $rel ) ? ( $rel."-".$uInfo->id ) : $uInfo->id ;
+                    yii::error($model->user_rels);
                     $model->on( ActiveRecord::EVENT_AFTER_INSERT, function( $ent ) use ( $id ){
                         $rel = new MgUserRel();
                         $rel->user_id = $id;
