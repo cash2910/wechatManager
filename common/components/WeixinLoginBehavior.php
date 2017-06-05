eb855fb4e46730a1aad2b6af9a038e7130641aee
zaixing.jiang <zaixing.jiang@dalegames.net>
2017-06-05 16:23:13 +0800

    init

--- common/components/WeixinLoginBehavior.php
+++ common/components/WeixinLoginBehavior.php
@@ -13,8 +13,9 @@ class WeixinLoginBehavior extends ActionFilter{
         $token = WeixinWeb::getInstance()->getClient()->getAccessToken();
         if( !$token ){
             $url = Yii::$app->urlManager->createAbsoluteUrl(['/Wechat'] );
-            $returl =  WeixinWeb::getInstance()->getClient()->setReturnUrl( $url )->buildAuthUrl();
-            Yii::$app->getResponse()->redirect($returl);
+            WeixinWeb::getInstance()->getClient()->setReturnUrl( $url );
+            $returl =  WeixinWeb::getInstance()->getClient()->buildAuthUrl();
+            Yii::$app->getResponse()->redirect( $returl );
             return false;
         }
         return true;
