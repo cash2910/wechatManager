f57030c22e4a6093a901ffecdb677b81d55ebd47
zaixing.jiang <zaixing.jiang@dalegames.net>
2017-06-05 17:44:49 +0800

    init

--- frontend/modules/Wechat/views/default/my_friend.php
+++ frontend/modules/Wechat/views/default/my_friend.php
@@ -3,7 +3,7 @@
         <?php foreach ($subs as $sub):?>
         <div class="weui-cell">
             <div class="weui-cell__hd" style="position: relative;margin-right: 10px;">
-                <img src="<?=$user['user_logo'] ?>" style="width: 50px;display: block">
+                <img src="<?=$sub['user_logo'] ?>" style="width: 50px;display: block">
                <!--  <span class="weui-badge" style="position: absolute;top: -.4em;right: -.4em;">8</span>  -->
             </div>
             <div class="weui-cell__bd">
