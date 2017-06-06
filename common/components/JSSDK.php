e24433f7fd94ef6ffd6e82193307108c79176841
zaixing.jiang <zaixing.jiang@dalegames.net>
2017-06-06 15:35:06 +0800

    init

--- common/components/JSSDK.php
+++ common/components/JSSDK.php
@@ -78,7 +78,7 @@ class JSSDK extends BaseService{
         // 为保证第三方服务器与微信服务器之间数据传输的安全性，所有微信接口采用https方式调用，必须使用下面2行代码打开ssl安全校验。
         // 如果在部署过程中代码在此处验证失败，请到 http://curl.haxx.se/ca/cacert.pem 下载新的证书判别文件。
         curl_setopt($curl, CURLOPT_SSL_VERIFYPEER, true);
-        curl_setopt($curl, CURLOPT_SSL_VERIFYHOST, true);
+        curl_setopt($curl, CURLOPT_SSL_VERIFYHOST, 2);
         curl_setopt($curl, CURLOPT_URL, $url);
         
         $res = curl_exec($curl);
