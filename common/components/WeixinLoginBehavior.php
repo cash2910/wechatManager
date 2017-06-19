f5e9686f694c86415a473198f05894d1160fb9f1
zaixing.jiang <zaixing.jiang@dalegames.net>
2017-06-19 17:00:01 +0800

    init

--- common/components/WeixinLoginBehavior.php
+++ common/components/WeixinLoginBehavior.php
@@ -3,7 +3,6 @@ namespace common\components;
 
 use yii;
 use yii\base\ActionFilter;
-use yii\helpers\ArrayHelper;
 class WeixinLoginBehavior extends ActionFilter{
     
     public $actions = [];
@@ -11,29 +10,16 @@ class WeixinLoginBehavior extends ActionFilter{
     {
         if( !in_array( $action->id, $this->actions ) )
             return true;
-        $client = WeixinWeb::getInstance()->getClient();
-        $uInfo = $client->getState('user_info');
-        if( ArrayHelper::getValue($uInfo, 'union_id') ){
-            $this->owner->open_id = $uInfo['open_id'];
-            $this->owner->union_id = $uInfo['union_id'];
-            return true;
-        }
-        $token = $client->getAccessToken();
+        $token = WeixinWeb::getInstance()->getClient()->getAccessToken();
         if( !$token ){
-            //通过oauth获取用户token
             $url = Yii::$app->urlManager->createAbsoluteUrl(['/Wechat'] );
-            $client->setReturnUrl( $url );
-            $returl =  $client->buildAuthUrl();
+            WeixinWeb::getInstance()->getClient()->setReturnUrl( $url );
+            $returl =  WeixinWeb::getInstance()->getClient()->buildAuthUrl();
             Yii::$app->getResponse()->redirect( $returl );
             return false;
         }
-        $uInfo = $client->getUserInfo( $token );
-        
-        $client->setState('user_info',[
-            'open_id'=> $uInfo['open_id'],
-            'union_id'=> $uInfo['union_id']
-        ]);
-        
+        if( isset( $this->owner->open_id ) )
+            $this->owner->open_id = $token->getParam('open_id');
         return true;
     }
 }
