67bf23f0b6de1912ea5fc027dcc1c5f8dce46eec
zaixing.jiang <zaixing.jiang@dalegames.net>
2017-06-28 14:33:39 +0800

    init

--- frontend/modules/Wechat/views/default/my_charge.php
+++ frontend/modules/Wechat/views/default/my_charge.php
@@ -1,37 +1,40 @@
-<div class="weui-flex" style="position: absolute;
+<div  style="position: absolute;
     top: 0;
     background-color: #f8f8f8;
     bottom: 0;
+    padding: 0 15px;
     left: 0;
     right: 0;">
-    <div class="weui-flex__item" style="margin:20px 20px;">
-        <div class="weui-panel__hd" >
-            <div style="color: #333;">充值游戏</div>
-        </div>
-       <div class="weui-flex">
-           <div class="weui-cells__tips" style="margin-top: 10px;">充值数量</div>
-       </div>
-       <div class="weui-flex" style="">
-            <div class="weui-flex__item"><a href="javascript:;" class="weui-btn weui-btn_mini  weui-btn_primary" style="font-size: 17px;">1000</a></div>
-            <div class="weui-flex__item"><a href="javascript:;" class="weui-btn weui-btn_mini weui-btn_default" style="font-size: 17px;">2000</a></div>
-            <div class="weui-flex__item"><a href="javascript:;" class="weui-btn weui-btn_mini weui-btn_default" style="font-size: 17px;">5000</a></div>
-            <div class="weui-flex__item"><a href="javascript:;" class="weui-btn weui-btn_mini weui-btn_default" style="font-size: 17px;">10000</a></div>
-        </div>
-        <div class="weui-flex" >
-             <p >50.6元（95折）</p>
-        </div>
-        <div  class="weui-flex__hd">
-            <div class="">
-                <a class="weui-btn weui-btn_primary" href="javascript:" id="showTooltips">充值</a>
+    <div class="weui-flex" style="margin-top: 5px;" >
+         <h4 class="title" style="color: #333;" >充值游戏</h4>
+    </div>  
+    <div class="weui-flex">
+        <div class="weui-flex__item" >
+           <div class="weui-flex">
+               <div class="weui-cells__tips" style="margin-top: 10px;">充值数量</div>
+           </div>
+           <div class="weui-flex" >
+                <div class="weui-flex__item"><a href="javascript:;" class="weui-btn weui-btn_mini  weui-btn_primary" style="font-size: 17px;">1000</a></div>
+                <div class="weui-flex__item"><a href="javascript:;" class="weui-btn weui-btn_mini weui-btn_default" style="font-size: 17px;">2000</a></div>
+                <div class="weui-flex__item"><a href="javascript:;" class="weui-btn weui-btn_mini weui-btn_default" style="font-size: 17px;">5000</a></div>
+                <div class="weui-flex__item"><a href="javascript:;" class="weui-btn weui-btn_mini weui-btn_default" style="font-size: 17px;">10000</a></div>
+            </div>
+            <div class="weui-flex" >
+                 <p >50.6元（95折）</p>
             </div>
         </div>
     </div>
+     <div class="weui-flex">
+        <div class="weui-flex__item">
+            <a class="weui-btn weui-btn_primary" href="javascript:" id="showTooltips">充值</a>
+        </div>
+     </div>
 </div>
 <script>
 $("#_total").focus();
 </script>
 <style>
-.weui-flex.weui-flex__item a {
-    font-size: 17px;
+.weui-flex .weui-flex__item {
+    margin-right: 0.2em;
 }
 </style>
