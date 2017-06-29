c157125db8fb5f9bb5df1c6a68847cd5016a5b58
zaixing.jiang <zaixing.jiang@dalegames.net>
2017-06-29 09:59:29 +0800

    init

--- frontend/modules/Wechat/views/default/my_charge.php
+++ frontend/modules/Wechat/views/default/my_charge.php
@@ -16,7 +16,8 @@
            <div class="weui-flex game_goods" >
                 <?php foreach($goods as $id => $good):?>
                 <!--  weui-btn_primary  -->
-                <div class="weui-flex__item"><a href="javascript:;" class="weui-btn weui-btn_mini weui-btn_primary" ><?php echo $good->title?></a></div>
+               <a href="javascript:;" class="weui-btn weui-btn_mini " ><?php echo $good->title?></a>
+                <a href="javascript:;" class="weui-btn weui-btn_mini " ><?php echo $good->title?></a>
                 <?php endforeach;?>
             </div>
             <div class="weui-flex" style="margin-top: 10px;">
@@ -32,10 +33,14 @@
 </div>
 <script>
 $(function(){
-/* 	$(".game_goods .weui-flex__item").click(function(){
-		$(this).removeClass("weui-btn_default").addClass("weui-btn_primary");
-		$(this).sublings().removeClass("weui-btn_primary").addClass("weui-btn_default");
-	}); */
+	$(".game_goods .weui-flex__item").click(function(){
+		$(this).find("a").addClass("weui-btn_primary");
+		$(this).siblings().each(function(){
+			$(this).find("a").removeClass("weui-btn_primary");
+	    })
+		
+	    
+	});
 });
 
 </script>
@@ -47,6 +52,11 @@ $(function(){
 	font-size: 17px;
 	color: #000;
     background-color: #f8f8f8;
+}
+
+.weui-flex .weui-flex__item  .weui-btn_primary{
+	background-color: #1aad19;
+	color: #fff;
 }
 .sp_total{
 	font-size: 25px;
