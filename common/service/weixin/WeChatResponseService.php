619cccaa214df7502ace6904ca3f8b86a32fe157
tom <glory_jzx@sina.com>
2017-08-03 18:45:54 +0800

    ubut

--- common/service/weixin/WeChatResponseService.php
+++ common/service/weixin/WeChatResponseService.php
@@ -67,17 +67,28 @@ class WeChatResponseService extends Module{
             }
             
              $msg = <<<EOF
- 客官，终于等到您了，欢迎来到人人麻将！
-    1、	下载游戏请<a href="http://wx.menguanol.net/Wechat/default/game-page">点击</a>
-    2、	提交BUG请回复咨询客服
-    3、	游戏充值<a href="http://wx.menguanol.net/Wechat/default/my-index">点击</a>
-    4、	进入我的后台 <a href="http://wx.menguanol.net/Wechat/default/my-index">点击</a>
-    5、	立刻生成自己<a href="http://wx.menguanol.net/Wechat/default/share-page">专属二维码</a>，推广下线。
- 我们正在招兵买马，全国招收代理：代理可享受以下政策
-    1、	可查看好友账单明细与提现等操作。
-    2、	生成自己专属二维码，方便推广。
-    3、	成功绑定下级，享受名下玩家消费返利。
-    4、	免费培训，帮助代理躺着赚钱。
+客官，终于等到您了，欢迎关注人人麻将公众号！
+
+点击：下载游戏
+
+点击：提交BUG奖励元宝
+
+点击：充值元宝
+
+点击：代理后台
+
+点击：邀请好友组局（生成自己专属二维码，推广下线）
+
+
+我们正在招兵买马，全国范围内招收代理：代理可享受以下政策
+
+1、可查看好友账单明细与提现等操作。
+      
+2、生成自己专属二维码，方便推广。
+
+3、成功绑定下级，享受名下玩家消费返利。
+
+4、免费培训，帮助代理躺着赚钱。 
 EOF;
              $entity->setResp([
                  'FromUserName'=>$entity->ToUserName,
