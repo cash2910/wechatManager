e0dc324b8936e861b02f80a13e1da91f899849e2
tom <glory_jzx@sina.com>
2017-08-03 10:06:18 +0800

    init

--- frontend/modules/Game/controllers/DefaultController.php
+++ frontend/modules/Game/controllers/DefaultController.php
@@ -22,6 +22,19 @@ class DefaultController extends Controller
      */
     public function actionIndex()
     {
+            $msg = <<<EOF
+                               客官，终于等到您了，欢迎来到人人麻将！
+                1、	下载游戏请<a href="http://wx.menguanol.net/Wechat/default/game-page">点击</a>
+                2、	提交BUG请回复咨询客服
+                3、	游戏充值<a href="http://wx.menguanol.net/Wechat/default/my-index">点击</a>
+                4、	进入我的后台 <a href="http://wx.menguanol.net/Wechat/default/my-index">点击</a>
+                5、	立刻生成自己<a href="http://wx.menguanol.net/Wechat/default/share-page">专属二维码</a>，推广下线。
+                                我们正在招兵买马，全国招收代理：代理可享受以下政策
+                1、	可查看好友账单明细与提现等操作。
+                2、	生成自己专属二维码，方便推广。
+                3、	成功绑定下级，享受名下玩家消费返利。
+                4、	免费培训，帮助代理躺着赚钱。
+EOF;
        // var_dump(yii::$app->request->url);
     }
     
