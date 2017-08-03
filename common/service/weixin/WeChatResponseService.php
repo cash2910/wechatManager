0fd69f11624c914363238be0076e52a112c436ba
tom <glory_jzx@sina.com>
2017-08-03 10:11:20 +0800

    init

--- common/service/weixin/WeChatResponseService.php
+++ common/service/weixin/WeChatResponseService.php
@@ -102,11 +102,24 @@ EOF;
                     'status'=> 1,
                 ]);
                 //通知用户
+                 $msg = <<<EOF
+                               客官，终于等到您了，欢迎来到人人麻将！
+                1、	下载游戏请<a href="http://wx.menguanol.net/Wechat/default/game-page">点击</a>
+                2、	提交BUG请回复咨询客服
+                3、	游戏充值<a href="http://wx.menguanol.net/Wechat/default/my-index">点击</a>
+                4、	进入我的后台 <a href="http://wx.menguanol.net/Wechat/default/my-index">点击</a>
+                5、	立刻生成自己<a href="http://wx.menguanol.net/Wechat/default/share-page">专属二维码</a>，推广下线。
+                                我们正在招兵买马，全国招收代理：代理可享受以下政策
+                1、	可查看好友账单明细与提现等操作。
+                2、	生成自己专属二维码，方便推广。
+                3、	成功绑定下级，享受名下玩家消费返利。
+                4、	免费培训，帮助代理躺着赚钱。
+EOF;
                 $entity->setResp([
-                    'FromUserName'=> $entity->ToUserName,
-                    'ToUserName'=> $open_id,
-                    'Content'=>"欢迎回来~",
-                    'MsgType' =>'text',
+                    'FromUserName'=>$entity->ToUserName,
+                    'ToUserName'=>$entity->FromUserName,
+                    'MsgType'=>'text',
+                    'Content'=>$msg
                 ]);
                 return true;
             }
