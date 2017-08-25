fa60933de7ce721695c48d53738865275ab14dad
tom <glory_jzx@sina.com>
2017-08-25 15:00:58 +0800

    init

--- common/components/WeixinLoginBehavior.php
+++ common/components/WeixinLoginBehavior.php
@@ -10,21 +10,22 @@ class WeixinLoginBehavior extends ActionFilter{
     public function beforeAction($action)
     {
         //测试用途；
-          if( in_array( Yii::$app->request->userIP, ['127.0.0.1'] ) ){
-            //$this->owner->open_id = 'o9Unv0a0sL-H8lREpQ86O5WodVyg';
-            $this->owner->open_id = 'o9Unv0RVvjPNBhde4LI68AYoRUiA';
+/*           if( in_array( Yii::$app->request->userIP, ['127.0.0.1'] ) ){
+            $this->owner->open_id = 'o9Unv0a0sL-H8lREpQ86O5WodVyg';
+            //$this->owner->open_id = 'o9Unv0RVvjPNBhde4LI68AYoRUiA';
             return true;
-        }
+        } */
         if( !in_array( $action->id, $this->actions ) )
             return true;
         $token = WeixinWeb::getInstance()->getClient()->getAccessToken();
         if( !$token ){
-            $url = Yii::$app->urlManager->createAbsoluteUrl(['/Wechat','r_url'=>yii::$app->request->url ] );
+            $url = urlencode( Yii::$app->urlManager->createAbsoluteUrl(['/Wechat','r_url'=> yii::$app->request->url  ] ) ); 
             $clientObj = WeixinWeb::getInstance()->getClient();
             $clientObj->setReturnUrl( $url );
             if( in_array( $action->id, $this->detail_actions ) )
                 $clientObj->scope = 'snsapi_userinfo';
             $returl =  $clientObj->buildAuthUrl();
+            die( $returl );
             Yii::$app->getResponse()->redirect( $returl );
             return false;
         }
