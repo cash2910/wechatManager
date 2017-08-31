d3ffa0e8d4557cbcf30abefe8df6113293131306
root <root@c6-php02-1-102.mengguan.com>
2017-08-31 14:23:49 +0800

    init

---
+++ environments/prod_xz/common/config/params-local.php
@@ -0,0 +1,32 @@
+<?php
+return [
+    'adminEmail' => 'admin@example.com',
+    'supportEmail' => 'support@example.com',
+    'user.passwordResetTokenExpire' => 3600,
+    //MG竞技 （老）
+    //'AppId' => 'wx33a109a15b3f3302',
+    //'AppSecret'=>'7c3ae345f52e87945cf655c1f24ad1e7',
+    //'MCHID' =>'', 
+    //MG互娱 (新) //重庆
+    //'AppId' => 'wxc41b5bd89d328132',
+    //'AppSecret'=>'4be0b3d8ff64babcb1559c6f091e092b',
+    //'MCHID' =>'1483656542', 
+    //万州麻将
+    //'AppId' => 'wxa5fc45698fa938c4',
+    //'AppSecret'=>'baf231fd4587f8370c144577f956a7e6',
+    //'MCHID' =>'1485129652',
+      
+    //血战
+    'AppId' => 'wx504fbccb58b2f158',
+    'AppSecret'=>'5822c1d4ac5a010d631ce8fb9905bfc9',
+    'MCHID' =>'1485129653',
+
+
+    'DEFAULT_USER'=>322, //默认最终用户ID
+	
+    'WEIXIN_ID' =>'renren_wz',   
+    'my_index'=>'http://wxxz.menguanol.net/Wechat/default/my-index',
+    //游戏地址
+    'GAME_URL'=>'http://116.62.113.199:9321',
+    'CERT_PATH' => '/web/cert/',
+];
