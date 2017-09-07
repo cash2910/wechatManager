8661e27358fc51721806c60d7bd290f9523d324a
notebook <test>
2017-09-07 17:19:18 +0800

    init

--- common/service/weixin/WeChatProxyService.php
+++ common/service/weixin/WeChatProxyService.php
@@ -8,6 +8,8 @@ use common\utils\Curl;
 use yii\helpers\ArrayHelper;
 class WeChatProxyService{
     
+    static public $hasTry = false;
+    
     public function  __call( $method, $params ){
         $conf = WeixinConfig::getConf( $method );
         $arg = isset( $params[0] ) ? $params[0] : [];
@@ -19,20 +21,32 @@ class WeChatProxyService{
     
     protected function process( $conf, $params ){
      
-        $finalUrl = $this->buildUrl( $conf, $params );
-        if( !$finalUrl )
-            throw new Exception("invalid wechat url...");
-        $curl = new Curl();
-        $curl->setOptions([ CURLOPT_SSL_VERIFYPEER=> false,  CURLOPT_SSL_VERIFYHOST => 0 ]);
-        if( ArrayHelper::getValue($conf, 'isPost', false) ){
-            $res =  $curl->setOption(CURLOPT_POSTFIELDS, json_encode($params, JSON_UNESCAPED_UNICODE) )->post( $finalUrl );
-        }else{
-            $res = $curl->get( $finalUrl );
+        try{
+            $finalUrl = $this->buildUrl( $conf, $params );
+            if( !$finalUrl )
+                throw new Exception("invalid wechat url...");
+            $curl = new Curl();
+            $curl->setOptions([ CURLOPT_SSL_VERIFYPEER=> false,  CURLOPT_SSL_VERIFYHOST => 0 ]);
+            if( ArrayHelper::getValue($conf, 'isPost', false) ){
+                $res =  $curl->setOption(CURLOPT_POSTFIELDS, json_encode($params, JSON_UNESCAPED_UNICODE) )->post( $finalUrl );
+            }else{
+                $res = $curl->get( $finalUrl );
+            }
+            $ret = json_decode( $res, true );
+            //统一判断处理异常  防止请求access_token 重启
+            if( ArrayHelper::getValue($ret, 'errcode') == 40001 && !self::$hasTry ){
+                yii::error('token失效：'. ArrayHelper::getValue($ret, 'errmsg'));
+                //重新获取token
+                $token = self::getToken( true );
+                $ret = self::process(  $conf, $params );
+            }
+            if( ArrayHelper::getValue($ret, 'errcode') != 0 ){
+                throw new \Exception( ArrayHelper::getValue($ret, 'errmsg') );
+            }
+        }catch ( \Exception $e){
+            $ret = [ 'isOk'=>0, 'msg'=>$e->getMessage() ];
         }
-        //统一判断处理异常
-        //var_dump( $res );
-        
-        return json_decode( $res, true );
+        return $ret;
     }
     
     protected function buildUrl( $conf, $params ){
@@ -65,7 +79,7 @@ class WeChatProxyService{
              ]);
              $token = ArrayHelper::getValue($res, 'access_token');
              if( !$token )
-                 throw new Exception( $res['errmsg'] );
+                 throw new Exception( ArrayHelper::getValue($res, 'msg') );
              yii::$app->redis->set( WeixinConfig::TOKEN_KEY, $token, 'EX', 7000 );
          }
          return $token;
