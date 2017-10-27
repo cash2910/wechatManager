6d0553f2892da504581038fb32e157b78c536ec1
root <root@c6-php04-1-104.mengguan.com>
2017-10-27 09:01:22 +0800

    init

--- common/config/main.php
+++ common/config/main.php
@@ -1,6 +1,7 @@
 <?php
 return [
     'vendorPath' => dirname(dirname(__DIR__)) . '/vendor',
+    'language' => 'zh-CN',
     'components' => [
         'cache' => [
             'class' => 'yii\caching\FileCache',
